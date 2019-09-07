c68e67765879b6c925d77f1667517c79c678223a
Nicholas <nm9ur@virginia.edu>
2019-09-07 12:41:05 -0400

    Cleaned up bitmoji imgs

--- js/random_bitmoji.js
+++ js/random_bitmoji.js
@@ -1,5 +1,6 @@
 function rand_bitmoji() {
-	bit_id = Math.floor(Math.random()*6);
-	document.getElementById("bitmoji").innerHTML = "<img src=/imgs/bitmojis/bitmoji" + bit_id;
+	bit_id = Math.floor(Math.random()*5+2);
+	document.getElementById("about-blurb").innerHTML += '<img align="right" src=imgs/bitmojis/bitmoji' + bit_id + '.png style="float: left">'
+	//document.getElementById("bitmoji").innerHTML += '<img align="right" src=imgs/bitmojis/bitmoji' + bit_id + '.png style="float: left">'
 }
 window.onload = rand_bitmoji;
