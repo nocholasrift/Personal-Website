fbee7595e13440723a081f94611774bbd7db9ed5
Nicholas <nickmihammad12665@gmail.com>
2019-09-08 14:54:08 -0400

    Updates

--- js/random_bitmoji.js
+++ js/random_bitmoji.js
@@ -1,6 +1,6 @@
 function rand_bitmoji() {
 	bit_id = Math.floor(Math.random()*5+2);
-	document.getElementById("about-blurb").innerHTML += '<img align="right" src=imgs/bitmojis/bitmoji' + bit_id + '.png style="float: left">'
+	document.getElementById("about-picture").innerHTML = '<img src="imgs/bitmojis/bitmoji' + bit_id + '.png">'
 	//document.getElementById("bitmoji").innerHTML += '<img align="right" src=imgs/bitmojis/bitmoji' + bit_id + '.png style="float: left">'
 }
 window.onload = rand_bitmoji;
